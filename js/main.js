23c992ffde0f881c9b01ef573c48e640fd5bf5c7
John Paul Kpantey <Jpbroadvision@users.noreply.github.com>
2020-12-09 13:37:24 +0000

    Updated the website

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
 (function($) {
     "use strict";
 
-    // Back to top button
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 100) {
-            $('#back-to-top').fadeIn('slow');
-        } else {
-            $('#back-to-top').fadeOut('slow');
-        }
-    });
+
 
     // $('#back-to-top').click(function() {
     //     $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
@@ -282,6 +275,14 @@
             e.preventDefault();
         });
     });
+    // Back to top button
+    // $(window).scroll(function() {
+    //     if ($(this).scrollTop() > 100) {
+    //         $('#back-to-top').fadeIn('slow');
+    //     } else {
+    //         $('#back-to-top').fadeOut('slow');
+    //     }
+    // });
     $('#back-to-top').on('click', function(e) {
         e.preventDefault();
         $('html,body').animate({
